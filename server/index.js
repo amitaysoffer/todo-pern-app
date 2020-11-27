f967c1eccf0cf9b77a06c9b5561aa49f5899b983
amitaysoffer <amitaysof@gmail.com>
2020-11-27 12:25:54 +0000

    first commit

---
+++ server/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+// const path = require('path');
+const PORT = process.env.PORT || 5000;
+const pool = require('./db')
+
+// middleware
+// app.use(cors()) ?? 
+app.use(express.json()) // to get data from the client side we need to use req.body and this allows us to access the req.body and get json data.
+
+// ROUTES \\\
+// create a todo
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body
+    const newTodo = await pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [description]);
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+// get all todos
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo');
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
+// get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
+// update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body
+    const updateTodo = await pool.query('UPDATE todo SET description = $1 WHERE todo_id = $2', [description, id]);
+    res.json('Todo was updated')
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+// delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+    res.json('Todo was deleted!')
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
